44d48370b7c1bd690dc64ee75b6f64ac77823f8d
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-08-12 22:43:39 -0500

    adding background

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 var canvas = document.getElementById("myCanvas");
 var render = canvas.getContext("2d");
+var width;
+var height;
+width = window.innerWidth;
+height = winder.innerHeight;
 
+canvas.width = width;
+canvas.height = height;
+canvas.style.left = "0px";
+canvas.style.top = "0px";
+canvas.style.position = "absolute";
+
+background(25, 25, 25);
+
+fillColor(155, 50, 255);
+strokeColor(0, 255, 0);
+strokeSize(2);
 rectangle(25, 25, 50, 100);
