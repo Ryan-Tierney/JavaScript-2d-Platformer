b56bafce10dbbadea7be3a53345913e712b912f3
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-08-13 22:22:10 -0500

    added floor

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var render = canvas.getContext("2d");
 var width;
 var height;
 width = window.innerWidth;
-height = winder.innerHeight;
+height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
@@ -11,9 +11,34 @@ canvas.style.left = "0px";
 canvas.style.top = "0px";
 canvas.style.position = "absolute";
 
+window.onresize = function () {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+  background(25, 25, 25);
+
+  noFill();
+  strokeColor(0, 255, 0);
+  strokeSize(2);
+  rectangle(25, 698, 50, 100);
+  strokeColor(255, 0, 0);
+  line(0, 800, 2000, 800);
+  noStroke();
+  fillColor(0, 255, 255);
+  circle(1750, 725, 67);
+};
+
+window.onresize();
+
 background(25, 25, 25);
 
-fillColor(155, 50, 255);
+noFill();
 strokeColor(0, 255, 0);
 strokeSize(2);
-rectangle(25, 25, 50, 100);
+rectangle(25, 698, 50, 100);
+strokeColor(255, 0, 0);
+line(0, 800, 2000, 800);
+strokeColor(255, 255, 255);
+fillColor(0, 0, 255);
+circle(1750, 725, 67);
